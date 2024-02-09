65a7959a4140c5eebf82d25e7ee55a8dceba01d6
archit-10 <architvnt@gmail.com>
2024-02-09 18:38:47 +0530

    updated

---
+++ Transpose_of_amatrix/code.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void transpose(vector<vector<int>> &matrix, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+}
