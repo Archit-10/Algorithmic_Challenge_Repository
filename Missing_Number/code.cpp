f5a6393022e70a2365a37d713cc5d6873b962264
archit-10 <architvnt@gmail.com>
2024-02-20 12:01:16 +0530

    updated

--- Missing_Number/code.cpp
+++ Missing_Number/code.cpp
@@ -2,3 +2,20 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int n = nums.size();
+        int sum = n * (n + 1) / 2;
+        int nsum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            nsum += nums[i];
+        }
+        int ans = sum - nsum;
+        return ans;
+    }
+};
