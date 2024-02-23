69238ffad522fa026cf3fc28127d95d7b24ed534
archit-10 <architvnt@gmail.com>
2024-02-23 12:36:41 +0530

    updated

---
+++ Buy_And_Sell_Share_Atmost_Twice/code.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &price)
+    {
+        int n = price.size();
+        vector<int> dp(n, 0);
+        int ma = price[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+        {
+            dp[i] = max(dp[i + 1], ma - price[i]);
+            ma = max(ma, price[i]);
+        }
+        int mi = price[0];
+        for (int i = 1; i < n; i++)
+        {
+            dp[i] = max(dp[i - 1], dp[i] + price[i] - mi);
+            mi = min(mi, price[i]);
+        }
+
+        return (dp[n - 1]);
+    }
+};
