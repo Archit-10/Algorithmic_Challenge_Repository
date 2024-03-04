3bde46d5a0d1fc6835ed650942d5b2336f0be11f
archit-10 <architvnt@gmail.com>
2024-03-04 17:25:27 +0530

    updated

--- Bag_of_tokens/code.cpp
+++ Bag_of_tokens/code.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
 
+using namespace std;
+
+class Solution
+{
+public:
+    int bagOfTokensScore(vector<int> &tokens, int power)
+    {
+        sort(tokens.begin(), tokens.end());
+        int left = 0, right = tokens.size() - 1;
+        int maxScore = 0, currScore = 0;
+        while (left <= right)
+        {
+            if (power >= tokens[left])
+            {
+                currScore += 1;
+                power -= tokens[left++];
+            }
+            else
+            {
+                currScore -= 1;
+                power += tokens[right--];
+            }
+            if (currScore < 0)
+                break;
+            maxScore = max(currScore, maxScore);
+        }
+        return maxScore;
+    }
+};
