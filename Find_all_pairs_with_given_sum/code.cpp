6be42c7fdb677f23bffc70778b49c80bbbdf9454
archit-10 <architvnt@gmail.com>
2024-03-06 17:25:13 +0530

    updated

---
+++ Find_all_pairs_with_given_sum/code.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<pair<int, int>> allPairs(int A[], int B[], int N, int M, int X)
+    {
+        vector<pair<int, int>> ans;
+
+        unordered_map<int, int> mp1;
+        unordered_map<int, int> mp2;
+        for (int i = 0; i < N; i++)
+        {
+            mp1[A[i]]++;
+        }
+        for (int i = 0; i < M; i++)
+        {
+            mp2[B[i]]++;
+        }
+        for (auto it : mp1)
+        {
+            int sum = X - it.first;
+            if (mp2.find(sum) != mp2.end())
+            {
+                ans.push_back({it.first, sum});
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
