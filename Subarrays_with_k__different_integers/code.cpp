d91a1deb359cea1a9a4010525a19174bbf9f11c7
archit-10 <architvnt@gmail.com>
2024-03-30 10:46:49 +0530

    updated

--- Subarrays_with_k__different_integers/code.cpp
+++ Subarrays_with_k__different_integers/code.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
 
+using namespace std;
+
+class Solution
+{
+public:
+    int ans(vector<int> nums, int k)
+    {
+        int n = nums.size();
+
+        int i = 0, j = 0;
+        int cnt = 0;
+
+        map<int, int> mp;
+
+        while (i < n)
+        {
+            mp[nums[i]]++;
+
+            while (mp.size() > k)
+            {
+                auto it = mp.find(nums[j]);
+                it->second--;
+
+                if (it->second == 0)
+                    mp.erase(it);
+                j++;
+            }
+            cnt += (i - j + 1);
+            i++;
+        }
+
+        return cnt;
+    }
+
+    int subarraysWithKDistinct(vector<int> &nums, int k)
+    {
+        return ans(nums, k) - ans(nums, k - 1);
+    }
+};
