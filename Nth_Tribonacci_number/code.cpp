42cdadc6d9bc39c47ddd9d66f87cdc48d3058b63
archit-10 <architvnt@gmail.com>
2024-04-24 08:21:46 +0530

    updated

---
+++ Nth_Tribonacci_number/code.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int tribonacci(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        else if (n == 1 || n == 2)
+        {
+            return 1;
+        }
+        int a = 0, b = 1, c = 1;
+        int ans = 0;
+        for (int i = 3; i <= n; i++)
+        {
+            ans = a + b + c;
+            a = b;
+            b = c;
+            c = ans;
+        }
+        return ans;
+    }
+};
