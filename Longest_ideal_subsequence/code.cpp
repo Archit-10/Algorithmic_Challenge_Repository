05afc845145bf8f87900df050b4a34abb9a0c21d
archit-10 <architvnt@gmail.com>
2024-04-25 09:55:04 +0530

    updated

---
+++ Longest_ideal_subsequence/code.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int longestIdealString(string s, int k)
+    {
+        int dp[150] = {0}, maxLen = 0;
+        for (char c : s)
+        {
+            for (int i = c - k; i <= c + k; ++i)
+            {
+                if (i >= 'a' && i <= 'z')
+                {
+                    dp[c] = max(dp[c], dp[i]);
+                }
+            }
+
+            maxLen = max(++dp[c], maxLen);
+        }
+
+        return maxLen;
+    }
+};
