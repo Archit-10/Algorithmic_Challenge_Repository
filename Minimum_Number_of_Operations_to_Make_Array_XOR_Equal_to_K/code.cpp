52bccb3d0bb15456bf23280cd7b46ccfc1e3c010
archit-10 <architvnt@gmail.com>
2024-04-29 14:46:02 +0530

    updated

---
+++ Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K/code.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int minOperations(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        int cnt = 0;
+        for (int i = 0; i <= 30; i++)
+        {
+            int b = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (((nums[j] >> i) & 1) == 1)
+                    b = b ^ 1;
+            }
+            if (((k >> i) & 1) != b)
+                cnt++;
+        }
+        return cnt;
+    }
+};
