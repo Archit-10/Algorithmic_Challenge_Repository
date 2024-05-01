d96937b7dbbf27c0d8904d45f2368be701ff15ea
archit-10 <architvnt@gmail.com>
2024-05-01 18:43:27 +0530

    updated

---
+++ Reverse_Prefix_of_word/code.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string reversePrefix(string word, char ch)
+    {
+        for (int i = 0; i < word.size(); i++)
+        {
+
+            if (word[i] == ch)
+            {
+                int j = 0;
+
+                while (j < i)
+                    swap(word[j++], word[i--]);
+                break;
+            }
+        }
+
+        return word;
+    }
+};
