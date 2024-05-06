deb137431cf5b3dbf8027fef100c7b3647a7f2de
archit-10 <architvnt@gmail.com>
2024-05-06 15:46:49 +0530

    updated

---
+++ Remove_nodes_from_LinkedList/code.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *removeNodes(ListNode *head)
+    {
+        if (head == NULL)
+            return head;
+        head->next = removeNodes(head->next);
+        if (head->next == NULL)
+            return head;
+        if (head->next->val > head->val)
+            return head->next;
+        return head;
+    }
+};
