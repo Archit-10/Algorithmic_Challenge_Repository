fc87ad2e6458289d541856eaa8965c145620c40b
archit-10 <architvnt@gmail.com>
2024-05-31 11:24:50 +0530

    updated

---
+++ single_number_III/code.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> &nums)
+    {
+        unordered_set<int> res;
+        for (int num : nums)
+        {
+            if (res.find(num) == res.end())
+            {
+                res.insert(num);
+            }
+            else
+            {
+                res.erase(num);
+            }
+        }
+        return vector<int>(res.begin(), res.end());
+    }
+};
