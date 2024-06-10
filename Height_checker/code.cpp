39721fa63c6611cb3ca4803f1e059a073c373b0c
archit-10 <architvnt@gmail.com>
2024-06-10 09:26:58 +0530

    updated

---
+++ Height_checker/code.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int heightChecker(vector<int> &heights)
+    {
+
+        vector<int> v = heights;
+
+        sort(v.begin(), v.end());
+        int cnt = 0;
+
+        for (int i = 0; i < v.size(); i++)
+        {
+            if (v[i] != heights[i])
+                cnt++;
+        }
+        return cnt;
+    }
+};
