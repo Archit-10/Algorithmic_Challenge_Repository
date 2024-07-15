59616f02f01ae0143e5b4f74f6a2ca7aa84c67cd
archit-10 <architvnt@gmail.com>
2024-07-15 16:34:39 +0530

    updated

---
+++ Create_binary_tree_from_descriptions/code.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *createBinaryTree(vector<vector<int>> &descriptions)
+    {
+
+        set<int> s;
+        bool child[100001] = {};
+        unordered_map<int, TreeNode *> map;
+
+        for (auto d : descriptions)
+        {
+            if (map[d[0]] == nullptr)
+                map[d[0]] = new TreeNode(d[0]);
+            TreeNode *node = (map[d[1]] == nullptr ? new TreeNode(d[1]) : map[d[1]]);
+            if (d[2])
+                map[d[0]]->left = node;
+            else
+                map[d[0]]->right = node;
+            map[node->val] = node;
+            child[d[1]] = true;
+        }
+        for (auto &d : descriptions)
+            if (!child[d[0]])
+                return map[d[0]];
+        return nullptr;
+    }
+};
