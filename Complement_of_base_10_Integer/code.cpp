1d4e17d451ade4396ecaa507d12bfbba293b549d
archit-10 <architvnt@gmail.com>
2024-08-22 11:32:18 +0530

    updated

---
+++ Complement_of_base_10_Integer/code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int bitwiseComplement(int n)
+    {
+        if (n == 0)
+            return 1;
+        unsigned int mask = ~0;
+        while (n & mask)
+        {
+            mask <<= 1;
+        }
+        return ~n & ~mask;
+    }
+};
