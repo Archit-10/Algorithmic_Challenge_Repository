5b26b8d7dd34bbfbc06f6d1668e8e24623e75905
archit-10 <architvnt@gmail.com>
2024-09-26 13:26:29 +0530

    updated

---
+++ My_Calendar_I/code.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class MyCalendar
+{
+public:
+    vector<pair<int, int>> bookings;
+    MyCalendar()
+    {
+    }
+
+    bool book(int start, int end)
+    {
+
+        for (auto &event : bookings)
+        {
+            int existingStart = event.first;
+            int existingEnd = event.second;
+
+            if (start < existingEnd && end > existingStart)
+            {
+                return false;
+            }
+        }
+
+        bookings.push_back({start, end});
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
