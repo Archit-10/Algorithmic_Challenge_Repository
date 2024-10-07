e804450221bbf5378d073fa69e8ab73356512773
archit-10 <architvnt@gmail.com>
2024-10-08 00:04:30 +0530

    updated

---
+++ Minimum_String_Length_After_Removing_Substrings/code.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int minLength(string s)
+    {
+        stack<char> charStack;
+        for (char currentChar : s)
+        {
+            if (!charStack.empty() &&
+                ((charStack.top() == 'A' && currentChar == 'B') ||
+                 (charStack.top() == 'C' && currentChar == 'D')))
+            {
+                charStack.pop();
+            }
+            else
+            {
+                charStack.push(currentChar);
+            }
+        }
+        return charStack.size();
+    }
+};
