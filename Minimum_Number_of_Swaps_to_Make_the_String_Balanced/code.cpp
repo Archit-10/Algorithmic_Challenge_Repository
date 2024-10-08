729b46a39e0cc93e79c25ec9a4a48d81c0ca8af7
archit-10 <architvnt@gmail.com>
2024-10-08 09:56:55 +0530

    updated

---
+++ Minimum_Number_of_Swaps_to_Make_the_String_Balanced/code.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int minSwaps(string s)
+    {
+        int size = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++)
+        {
+            char ch = s[i];
+            if (ch == '[')
+                size++;
+            else if (size > 0)
+                size--;
+        }
+        return (size + 1) / 2;
+    }
+};
