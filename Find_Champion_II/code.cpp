9d82087d0ed616e67db6437f11f3e99d6b3cef27
archit-10 <architvnt@gmail.com>
2024-11-26 22:11:44 +0530

    updated

--- Find_Champion_II/code.cpp
+++ Find_Champion_II/code.cpp
@@ -3,32 +3,33 @@
 
 using namespace std;
 
-mclass Solution{
-    public :
-        int findChampion(int n, vector<vector<int>> &edges){
-            vector<int> in_degree(n, 0);
-
-for (auto edge : edges)
+class Solution
 {
-    int u = edge[0], v = edge[1];
-    in_degree[v]++;
-}
-
-int champion = 0;
-int ans = 0;
-for (int i = 0; i < n; i++)
-{
-    if (in_degree[i] == 0)
+public:
+    int findChampion(int n, vector<vector<int>> &edges)
     {
-        champion++;
-        ans = i;
-    }
-}
+        vector<int> in_degree(n, 0);
 
-if (champion > 1)
-    return -1;
-else
-    return ans;
-}
-}
-;
+        for (auto edge : edges)
+        {
+            int u = edge[0], v = edge[1];
+            in_degree[v]++;
+        }
+
+        int champion = 0;
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (in_degree[i] == 0)
+            {
+                champion++;
+                ans = i;
+            }
+        }
+
+        if (champion > 1)
+            return -1;
+        else
+            return ans;
+    }
+};
